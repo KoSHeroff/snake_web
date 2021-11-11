ed2bc903685a18bc59a28bf7a57aace437a593d3
Kirill Grinev <dev.kosher@gmail.com>
2021-11-11 07:40:05 +0700

    Last fix

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -1,5 +1,5 @@
 // variables
-const amountBlocks = 51;
+const amountBlocks = 41;
 var active = 'stop', action = 'stop', oldActive;
 var directions = { left: { x: -1, y: 0 }, right: { x: 1, y: 0 }, up: { x: 0, y: -1 }, down: { x: 0, y: 1 } };
 
